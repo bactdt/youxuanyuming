82ac72ebb1046541cea2d91d2b45c9aef910d46d
bactdt <81372089+bactdt@users.noreply.github.com>
2024-10-22 16:42:04 +0800

    Update collect_ips.py

--- collect_ips.py
+++ collect_ips.py
@@ -5,7 +5,8 @@ import os
 
 # 目标URL列表
 urls = ['https://ip.164746.xyz/ipTop10.html', 
-        'https://cf.090227.xyz'
+        ‘https://cm.yutian.us.kg’,
+        ‘https://test32.speed.xn--1jq721i.cn’
         ]
 
 # 正则表达式用于匹配IP地址
@@ -27,7 +28,9 @@ with open('ip.txt', 'w') as file:
         # 根据网站的不同结构找到包含IP地址的元素
         if url == 'https://ip.164746.xyz/ipTop10.html':
             elements = soup.find_all('tr')
-        elif url == 'https://cf.090227.xyz':
+        elif url == 'https://cm.yutian.us.kg':
+            elements = soup.find_all('tr')
+        elif url == 'https://test32.speed.xn--1jq721i.cn':
             elements = soup.find_all('tr')
         else:
             elements = soup.find_all('li')
