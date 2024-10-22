a818accfb650df01230b99e99e94a021541fe647
bactdt <81372089+bactdt@users.noreply.github.com>
2024-10-22 17:45:52 +0800

    Update collect_ips.py

--- collect_ips.py
+++ collect_ips.py
@@ -5,7 +5,7 @@ import os
 
 # 目标URL列表
 urls = [
-        'https://bihai.cf/CFIP/443.html',
+        'https://bihai.cf/CFIP/CMCC/standard.txt',
         'https://ip.164746.xyz/ipTop10.html'     
         ]
 
@@ -26,7 +26,7 @@ with open('ip.txt', 'w') as file:
         soup = BeautifulSoup(response.text, 'html.parser')
         
         # 根据网站的不同结构找到包含IP地址的元素
-        if url == 'https://bihai.cf/CFIP/443.html':
+        if url == 'https://bihai.cf/CFIP/CMCC/standard.txt',:
             elements = soup.find_all('tr')
         elif url == 'https://ip.164746.xyz/ipTop10.html':
             elements = soup.find_all('tr')
